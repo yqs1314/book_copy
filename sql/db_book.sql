9246644ec9c24041f45b0b7ea6791d541acee2ff
yqs <2416850394@qq.com>
2021-06-12 00:19:58 +0800

    b分支数据库

--- sql/db_book.sql
+++ sql/db_book.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : dbConnection
 Source Server Version : 50704
 Source Host           : localhost:3306
-Source Database       : db_book
+Source Database       : db_book  //数据库的名字
 
 Target Server Type    : MYSQL
 Target Server Version : 50704
